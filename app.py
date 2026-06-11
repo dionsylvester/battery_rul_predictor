55c1892329b5a74c7e96d249981d310cde81b324
dionsylvester <128568697+dionsylvester@users.noreply.github.com>
2026-06-11 20:20:29 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -169,3 +169,19 @@ with st.expander("View Model Insights"):
     }).sort_values(by="Importance", ascending=True)
     
     st.bar_chart(data=importance_data, x="Feature", y="Importance", horizontal=True)
+
+    st.divider()
+
+    st.markdown("The scatter plot comparing the predicted versus actual Remaining Useful Life values:")
+    
+    try:
+        st.image("results\scatterplot_gradient_boosting.png", caption="Scatter Plot of Gradient Boosting", use_container_width=True)
+    except:
+        st.warning("Scatter plot image file not found.")
+
+    st.markdown(
+        """
+        * **$R^2$ Score ($\t{RUL} \le 200$):** `0.7706`
+        * **$R^2$ Score ($\t{RUL} > 200$):** `0.9956`
+        """
+    )
